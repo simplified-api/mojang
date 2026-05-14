008adefaea4b2c73acd850e1c37e6ebbcd12aa48
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-05-14 15:44:16 -0400

    Migrate MojangApiException to ErrorContext signature

--- src/main/java/api/simplified/mojang/exception/MojangApiException.java
+++ src/main/java/api/simplified/mojang/exception/MojangApiException.java
@@ -1,13 +1,14 @@
 package api.simplified.mojang.exception;
 
 import com.google.gson.Gson;
+import dev.simplified.client.exception.ErrorContext;
 import dev.simplified.client.exception.JsonApiException;
 import org.jetbrains.annotations.NotNull;
 
 public final class MojangApiException extends JsonApiException {
 
-    public MojangApiException(@NotNull Gson gson, @NotNull String methodKey, @NotNull feign.Response response) {
-        super(methodKey, response, "Mojang");
+    public MojangApiException(@NotNull Gson gson, @NotNull ErrorContext context) {
+        super(context, "Mojang");
         this.resolve(gson, MojangErrorResponse.class);
     }
 
